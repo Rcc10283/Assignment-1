029326deabbc8b02e5ab3b8391896b6f01358062
Rcc10283 <rc10283@bu.edu>
2026-09-03 02:08:02 +0000

    Mini.c

--- mini.c
+++ mini.c
@@ -6,16 +6,22 @@
     Returns 11 if x > 3, otherwise returns 17.
 */
 unsigned long long sort(long long x) {
-    
+    if (x > 3) {
+        return 11;
+    } else {
+        return 17;
+    }
 }
 
-/*
-    Input: Two long long (64 bit) integers x and y.
-    Returns 1 if x > y, otherwise, if both x and y are greater than 3, returns 12.
-    All other cases return 33.
-*/
+
 unsigned long long branch(long long x, long long y) {
-    
+    if (x > y) {
+        return 1;
+    } else if (x > 3 && y > 3) {
+        return 12;
+    } else {
+        return 33;
+    }
 }
 
 /*
@@ -24,5 +30,12 @@ unsigned long long branch(long long x, long long y) {
     For example loop(3,2) = 3*2 + 2*1 = 8.
 */
 int loop(int x, int y) {
-    
+    int sum = 0;
+    while (x > 0 && y > 0) {
+        sum += x * y;
+        x--;
+        y--;
+    }
+
+    return sum;   
 }
